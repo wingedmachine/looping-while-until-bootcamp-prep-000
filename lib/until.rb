9ccca32b44ed7a490f1e9797ae4665f88aa0943e
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-18 00:44:34 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,7 @@
-def using_until
+def using_until(looping_string)
   levitation_force = 6
   until levitation_force = 10
-    puts "Wingardium Leviosa"
+    puts looping_string
     levitation_force +=1
   end
 end
-
